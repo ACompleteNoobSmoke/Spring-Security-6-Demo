788cd9083199dd5c23c52b693b8e0cc7bce559bc
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-02-20 07:18:21 -0600

    Updated gitignore

---
+++ src/main/java/com/noobsmoke/springsecure/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.noobsmoke.springsecure.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String homeController() {
+        return "Uyster Da Brewster";
+    }
+
+    @GetMapping("/greet")
+    public String greet(HttpServletRequest request) {
+        return "Welcome ACompleteNoobSmoke: " + request.getSession();
+    }
+}
